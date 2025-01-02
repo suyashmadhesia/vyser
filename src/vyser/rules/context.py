ebfb6d19b16c9729e556a8c24368ff1ae1a17932
Suyash Maddhessiya <suyashmadhesia@gmail.com>
2025-01-02 16:25:30 +0530

    feat: added feature for the directly parsing condition using handler names

--- src/vyser/rules/context.py
+++ src/vyser/rules/context.py
@@ -15,6 +15,7 @@ class RuleContext:
         self._rule_name = rule_name
         self._when_all_handlers = []
         self._when_any_handlers = []
+        self._condition_handlers = {}
 
     def __enter__(self):
         RuleFactory.register_rule_context(self)
@@ -34,6 +35,11 @@ class RuleContext:
             {self.CONDITIONS: conditions, self.HANDLER: func}
         )
 
+    def add_condition_callback(self, func: Callable, condition_key):
+        if not isinstance(condition_key, str):
+            raise TypeError("Condition key must be a string")
+        self._condition_handlers[condition_key] = func
+
     def add_when_any_rule_callback(self, func: Callable, args):
         conditions: List[Condition] = []
         for arg in args:
@@ -45,19 +51,37 @@ class RuleContext:
             {self.CONDITIONS: conditions, self.HANDLER: func}
         )
 
-    def _execute_conditions(self, handler, data, result_operator):
+    def _execute_rules(self, handler, data, result_operator):
         conditions = handler[self.CONDITIONS]
         result = [condition.evaluate(data) for condition in conditions]
         return result_operator(result)
 
     def execute_rule(self, data: dict):
         for handler in self._when_all_handlers:
-            result = self._execute_conditions(handler, data, all)
+            result = self._execute_rules(handler, data, all)
             if result:
                 handler[self.HANDLER](self)
 
         for handler in self._when_any_handlers:
-            result = self._execute_conditions(handler, data, any)
+            result = self._execute_rules(handler, data, any)
             if result:
                 handler[self.HANDLER](self)
-    
+
+    def _execute_conditions(self, conditions, data):
+        result = [condition.evaluate(data) for condition in conditions]
+        return all(result)
+
+    def execute_condition(self, conditions_data: List[dict], data: dict):
+        for condition_data in conditions_data:
+            conditions_strs = condition_data["value"]
+            condition_handler_name = condition_data["handler"]
+            condition_instances = [
+                Condition(condition_str)
+                for condition_str in conditions_strs
+                if isinstance(condition_str, str)
+            ]
+            condition_handler = self._condition_handlers.get(condition_handler_name)
+            if not condition_handler:
+                raise ValueError(f"Handler '{condition_handler_name}' not found")
+            if self._execute_conditions(condition_instances, data):
+                condition_handler(self)
--- src/vyser/rules/engine.py
+++ src/vyser/rules/engine.py
@@ -29,6 +29,19 @@ def when_any(context: RuleContext, *args: List[Condition]):
     return decorator
 
 
+def match_condition(context: RuleContext, arg: str):
+
+    def decorator(func: Callable):
+        context.add_condition_callback(func, arg)
+
+        def wrapper(c: RuleContext):
+            return func(c)
+
+        return wrapper
+
+    return decorator
+
+
 def set_context_attrs(object, data):
     for key, val in data.items():
         setattr(object, key, val)
@@ -40,7 +53,14 @@ def remove_context_attrs(object, data):
 
 
 def assert_rule(rule_name: str, data: dict):
-    context = RuleFactory.get_rule_context(rule_name)
+    context: RuleContext = RuleFactory.get_rule_context(rule_name)
     set_context_attrs(context, data)
     context.execute_rule(data)
+    remove_context_attrs(context, data)
+
+
+def assert_condition(rule_name: str, conditions: List[dict], data: dict):
+    context: RuleContext = RuleFactory.get_rule_context(rule_name)
+    set_context_attrs(context, data)
+    context.execute_condition(conditions, data)
     remove_context_attrs(context, data)
